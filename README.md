2c1c509a8519592cd25af493ce841eab46b2a60a
Zack Murry <zackk.murryy@gmail.com>
2022-03-13 20:21:19 -0500

    Remove old conditional for string declarations

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Stellar is a compiled language that is built using the LLVM compiler toolchain. 
 To compile a given file, execute the compiler with the name of the file to compile as an argument.
 This will produce a file named `output.o`, an object file that needs to be compiled into machine code.
 This is done using a C++ runner file (`runner.cpp`), which invokes the entry point of your program.
-To use the runner to compile your program, run `g++ runner.cp output.o -static` (`clang++` also works),
+To use the runner to compile your program, run `g++ runner.cpp output.o -static` (`clang++` also works),
 which will produce an executable file named `a.out`.
 
 ### Configure standard library
--- src/ASTVariableDeclaration.cpp
+++ src/ASTVariableDeclaration.cpp
@@ -8,30 +8,24 @@ llvm::Value* ASTVariableDeclaration::codegen(CodegenData data) {
     if (data.generics->count(type.type)) {
         type = data.generics->at(type.type);
     }
-    if (type.type == "s") {
-        auto val = value->codegen(data);
-        data.namedValues->insert({ name, val });
-        return val;
+    auto genericType = convertVariableTypeToString(type);
+    int ivt = getPrimitiveVariableTypeFromString(genericType);
+    llvm::Type *llvmType;
+    if (ivt != -1) {
+        llvmType = getLLVMTypeByPrimitiveVariableType((PrimitiveVariableType) ivt, data.context);
+    } else if (data.classes->count(genericType)) {
+        llvmType = llvm::PointerType::getUnqual(data.classes->at(genericType).type);
     } else {
-        auto genericType = convertVariableTypeToString(type);
-        int ivt = getPrimitiveVariableTypeFromString(genericType);
-        llvm::Type *llvmType;
-        if (ivt != -1) {
-            llvmType = getLLVMTypeByPrimitiveVariableType((PrimitiveVariableType) ivt, data.context);
-        } else if (data.classes->count(genericType)) {
-            llvmType = llvm::PointerType::getUnqual(data.classes->at(genericType).type);
-        } else {
-            cerr << "Error: unknown variable type " << genericType << endl;
-            exit(EXIT_FAILURE);
-        }
-        llvm::AllocaInst *alloca = data.builder->CreateAlloca(llvmType, nullptr, name);
-        data.builder->CreateStore(
-                value->codegen(data), alloca);
-        data.namedValues->insert({name, alloca});
-        if (data.classes->count(genericType)) {
-            data.objectTypes->insert({ name, genericType });
-        }
-        return alloca;
+        cerr << "Error: unknown variable type " << genericType << endl;
+        exit(EXIT_FAILURE);
     }
+    llvm::AllocaInst *alloca = data.builder->CreateAlloca(llvmType, nullptr, name);
+    data.builder->CreateStore(
+            value->codegen(data), alloca);
+    data.namedValues->insert({name, alloca});
+    if (data.classes->count(genericType)) {
+        data.objectTypes->insert({ name, genericType });
+    }
+    return alloca;
 }
 
